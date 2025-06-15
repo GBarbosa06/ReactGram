09c79b1d83a9d650523df4b6966b968269420af9
gbarbosa06 <andradegui2006@gmail.com>
2025-06-15 19:43:35 -0300

    conexão com mongoDB

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,3 +1,18 @@
 const moogose = require('mongoose');
 
 // Connect to MongoDB
+const dbUser = process.env.DB_USER;
+const dbPassword =  process.env.DB_PASSWORD;
+
+const conn = async () => {
+    try{
+        const dbConn = await moogose.connect(`mongodb+srv://${dbUser}:${dbPassword}@clusterreactgram.1bgtsfi.mongodb.net/?retryWrites=true&w=majority&appName=ClusterReactGram`);
+        console.log("MongoDB conectado");
+        return dbConn;
+    } catch (error) {
+        console.error("Conexão com MongoDB falhou:", error);
+        throw error;
+    }
+}
+conn();
+module.exports = conn;
