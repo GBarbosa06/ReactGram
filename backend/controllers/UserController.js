d2e22c8a3fa11d737f4c27ef82534a6a042f0246
gbarbosa06 <andradegui2006@gmail.com>
2025-06-24 19:59:34 -0300

    user registration

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -14,7 +14,33 @@ const generateToken = (id) => {
 
 // Register a new user and sign in
 const register = async (req, res) => {
-    res.send("Registro");
+    const {name, email, password} = req.body;
+
+    // Check if user exists
+    const user = await User.findOne({email});
+    if(user){
+        res.status(422).json({errors: ["Email já utilizado"]});
+        return;
+    }
+
+    // generate password hash
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    // Create user
+    const newUser = await User.create({
+        name, email, password: passwordHash
+    })
+
+    //Check if user was successfully createred and return the token
+    if(!newUser){
+        res.status(422).json({errors: ["Houver um erro, tente novamente mais tarde"]});
+        return
+    }
+    res.status(201).json({
+        _id: newUser._id,
+        token: generateToken(newUser._id),
+    })
 };
 
 module.exports = {
